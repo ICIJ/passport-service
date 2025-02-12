b12b666a8a4075a038eae9342d80ecfb0bf5da00
Clément Doumouro <clement.doumouro@gmail.com>
2025-02-12 12:27:39 +0100

    feature: accept list of path as pipeline input

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
-from passport_service.objects import DocMetadata, as_doc_metadata
+from passport_service.objects import (
+    DocMetadata,
+    as_doc_metadata,
+    parse_preprocessing_request,
+)
 from tests import TEST_DATA_DIR
 
 
@@ -21,4 +25,25 @@ def test_as_doc_metadata() -> None:
         DocMetadata(path=Path("passport.png"), extension=".png"),
     ]
 
+    assert doc_metadata == expected
+
+
+def test_parse_preprocessing_request() -> None:
+    # Given
+    data_dir = TEST_DATA_DIR / "passports"
+    docs = [
+        str(data_dir / "some/path/with.ext"),
+        str(data_dir / "some/other_path/with.another_ext"),
+    ]
+
+    # When
+    doc_metadata = parse_preprocessing_request(docs, data_dir=data_dir)
+
+    # Then
+    expected = [
+        DocMetadata(path=Path("some/path/with.ext"), extension=".ext"),
+        DocMetadata(
+            path=Path("some/other_path/with.another_ext"), extension=".another_ext"
+        ),
+    ]
     assert doc_metadata == expected
